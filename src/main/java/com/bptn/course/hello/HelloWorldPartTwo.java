2cac0625e283f5d15ff2054d867cdca735fe9380
Abisola-Abioye <abisola.abioye42@gmail.com>
2025-01-23 12:08:42 -0500

    Another hello world class created

--- src/main/java/com/bptn/course/hello/HelloWorldPartTwo.java
+++ src/main/java/com/bptn/course/hello/HelloWorldPartTwo.java
@@ -1,10 +1,9 @@
 package com.bptn.course.hello;
 
-public class HelloWorld {
+public class HelloWorldPartTwo {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		
+		System.out.println("Hello world part2!");
 
 	}
 
